c3bfa1e912f9324ede9a39d6b0652bbbba554642
Asqvella <asqvella@gmail.com>
2017-09-02 06:56:53 -0700

    new example with postincrement

--- postincrement/main.c
+++ postincrement/main.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	int count = 0;
 	printf("count = %d\n", count);
-	printf("count++ = %d\n", ++count);
+	printf("count++ = %d\n", count++);
 	printf("count = %d\n", count);
 	return 0;
 }
